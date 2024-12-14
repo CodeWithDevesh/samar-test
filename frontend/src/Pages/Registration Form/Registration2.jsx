2bcd6cf13aed08304f30c3acb40e84f036e6fe65
Devesh Agarwal <deveshagarwal652005@gmail.com>
2024-12-14 22:01:25 +0530

    fix

--- frontend/src/Pages/Registration Form/Registration2.jsx
+++ frontend/src/Pages/Registration Form/Registration2.jsx
@@ -186,7 +186,7 @@ const Registration2 = () => {
           </button>
           {step != lastStep && (
             <button
-              // disabled={!enNext}
+              disabled={!enNext}
               onClick={handleNext}
               className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2"
             >
